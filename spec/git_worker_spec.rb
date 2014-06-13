82b2fc3c7802ab3a635f27399315e71a729ed793
Carlos Sanchez <csanchez@maestrodev.com>
2014-06-13 11:45:42 +0200

    Refactor spec

--- spec/git_worker_spec.rb
+++ spec/git_worker_spec.rb
@@ -19,6 +19,16 @@ require 'spec_helper'
 
 describe MaestroDev::Plugin::GitWorker do
 
+  let(:clone_fields) {{
+    'branch' => 'master',
+    'path' => @test_wc_path,
+    'url' => @test_repo_path,
+    'composition' => 'test',
+    'composition_id' => 1
+  }}
+  let(:workitem) {{'fields' => fields}}
+  let(:default_path) { File.expand_path("~/wc/test-1") }
+
   before(:all) do
     Maestro::MaestroWorker.mock!
 
@@ -26,7 +36,6 @@ describe MaestroDev::Plugin::GitWorker do
     @test_wc_path = "/tmp/maestro-git-wc"
     @test_repo_path = "/tmp/maestro-git-repo"
     @other_repo_path = "/tmp/some-other-git-repo"
-    FileUtils.rm_rf @test_wc_path
     FileUtils.rm_rf @test_repo_path
 
     Dir::mkdir(@test_repo_path)
@@ -35,109 +44,93 @@ describe MaestroDev::Plugin::GitWorker do
   end
 
 #  it 'should use a default path when path is not set' do
-#    workitem = {'fields' => {'composition' => ' test a composition #$ ', 'composition_id' => 1}}
 #    @git_participant.expects(:workitem).at_least_once.returns(workitem)
-#    @git_participant.default_path.should eq(File.expand_path("~/wc/test-a-composition--1"))
+#    expect(@git_participant.default_path).to eq(File.expand_path("~/wc/test-a-composition--1"))
 #  end
  
+  before(:each) do
+    FileUtils.rm_rf default_path
+    FileUtils.rm_rf @test_wc_path
+  end
+
+  def initial_clone
+    subject.perform(:clone, {'fields' => clone_fields})
+    `cd #{@test_wc_path} && git config user.name Test && git config user.email test@example.com`
+    expect(subject.error).to be_nil
+  end
+
   describe 'clone()' do
-    before(:each) do
-      FileUtils.rm_rf @test_wc_path
+
+    let(:fields) { clone_fields }
+    before do
+      subject.perform(:clone, workitem)
     end
 
-    it 'should clone to default path when path is not set' do
-      workitem = {'fields' => {'branch' => 'master', 'url' => @test_repo_path, 'composition' => 'test', 'composition_id' => 1}}
+    context 'when path is not set' do
+      let(:fields) { super().reject{|k,v| k=='path'} }
 
-      subject.perform(:clone, workitem)
-      
-      workitem['fields']['__error__'].should be_nil
-      path = workitem['fields']['__context_outputs__']['repo_path']
-      path.should eq(File.expand_path("~/wc/test-1"))
-      File.exists?(path).should be_true
-      FileUtils.rm_rf path
+      it("should not error") { expect(subject.error).to be_nil, subject.output }
+      it("should use the default path") { expect(subject.read_output_value('repo_path')).to eq(default_path) }
+      it("should create the default path dir") { expect(File.exists?(default_path)).to be_true, subject.output }
     end
 
     it 'should clone some code' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-      
-      subject.perform(:clone, workitem)
-      
       modtime = File.mtime(@test_wc_path)
-      workitem['fields']['__error__'].should be_nil
-      File.exists?(@test_wc_path).should be_true
+      expect(subject.error).to be_nil
+      expect(File.exists?(@test_wc_path)).to be_true
       sleep(1)
       workitem['fields']['clean_working_copy'] = false
 
       # Do it again!
       subject.perform(:clone, workitem)
       
-      modtime.should eql(modtime = File.mtime(@test_wc_path))
-      workitem['fields']['__error__'].should be_nil
-      File.exists?(@test_wc_path).should be_true
+      expect(modtime).to eql(modtime = File.mtime(@test_wc_path))
+      expect(subject.error).to be_nil
+      expect(File.exists?(@test_wc_path)).to be_true
       
       workitem['fields']['clean_working_copy'] = true
         
       # One more time - should revert change      
       subject.perform(:clone, workitem)
       
-      modtime.should_not eql(modtime = File.mtime(@test_wc_path))
-      workitem['fields']['__error__'].should be_nil
-      File.exists?(@test_wc_path).should be_true
+      expect(modtime).not_to eql(modtime = File.mtime(@test_wc_path))
+      expect(subject.error).to be_nil
+      expect(File.exists?(@test_wc_path)).to be_true
     end
 
     it 'should get the latest reference' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-
-      subject.perform(:clone, workitem)
-
-      File.exists?(@test_wc_path).should be_true
-      
-      ref = workitem['fields']['__context_outputs__']['reference']
-      ref.should_not be_nil
-    end    
+      expect(File.exists?(@test_wc_path)).to be_true
+      expect(subject.read_output_value('reference')).not_to be_nil
+    end
 
     it "should set not needed if reference matches previous build" do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-
-      subject.perform(:clone, workitem)
-
-      File.exists?(@test_wc_path).should be_true
+      expect(File.exists?(@test_wc_path)).to be_true
       
-      workitem['fields']['__error__'].should be_nil
-      workitem['fields']['__previous_context_outputs__'] = {"reference"=>workitem['fields']['__context_outputs__']['reference']}
+      expect(subject.error).to be_nil
+      workitem['fields']['__previous_context_outputs__'] = {"reference"=>subject.read_output_value('reference')}
 
       subject.expects(:not_needed)
       subject.perform(:clone, workitem)
     end
     
     it 'should get the ticket from the latest commit' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-
-      subject.perform(:clone, workitem)
-
-      File.exists?(@test_wc_path).should be_true
-      
-      tickets = workitem['fields']['__context_outputs__']['tickets']
-      tickets.should == ['TICKET-1', 'FEATURE-1']
+      expect(File.exists?(@test_wc_path)).to be_true      
+      expect(subject.read_output_value('tickets')).to eq(['TICKET-1', 'FEATURE-1'])
     end    
 
-    it 'should detect error if clone some code fails' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => "http://repo.or.cz/asdfasdf/adfasdf.git"}}
-      
-      subject.perform(:clone, workitem)
-      
-      workitem['fields']['__error__'].should_not be_nil
-      workitem['fields']['__error__'].should eq('Error cloning repo')
-      workitem['__output__'].should match("http://repo.or.cz/asdfasdf/adfasdf.git/")
+    context 'when cloning fails' do
+      let(:fields) { super().merge({'url' => "http://repo.or.cz/asdfasdf/adfasdf.git"}) }
 
-      File.exists?(@test_wc_path).should be_false
+      it 'should detect error' do
+        expect(subject.error).not_to be_nil
+        expect(subject.error).to eq('Error cloning repo')
+        expect(subject.output).to match("http://repo.or.cz/asdfasdf/adfasdf.git/")
+
+        expect(File.exists?(@test_wc_path)).to be_false
+      end
     end
     
     it 'should pull instead of clone on the second call' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-
-      subject.perform(:clone, workitem)
-
       # ensure creation time is the same after a second clone
       readme = "#{@test_repo_path}/README"
       f = File.new(readme)
@@ -145,163 +138,68 @@ describe MaestroDev::Plugin::GitWorker do
       
       subject.perform(:clone, workitem)
 
-      workitem['fields']['__error__'].should be_nil
-      File.exists?(@test_wc_path).should be_true
-      File.exists?(readme).should be_true
-      time.should eql(f.ctime)
+      expect(subject.error).to be_nil
+      expect(File.exists?(@test_wc_path)).to be_true
+      expect(File.exists?(readme)).to be_true
+      expect(time).to eql(f.ctime)
     end
 
     it 'should fail to pull if existing repo points to different url' do
-      workitem = {'fields' => {'branch' => 'master', 'path' => @test_wc_path, 'url' => @test_repo_path}}
-
-      subject.perform(:clone, workitem)
-
       workitem['fields']['url'] = @other_repo_path 
       subject.perform(:clone, workitem)
 
-      workitem['fields']['__error__'].should include('squatting')
+      expect(subject.error).to include('squatting')
     end
 
   end
 
 
-
   describe 'branch()' do
-     before(:each) do
-       FileUtils.rm_rf @test_wc_path
-       workitem = {'fields' => {'branch' => "master", 'path' => @test_wc_path, 'url' => @test_repo_path}}
 
-       subject.perform(:clone, workitem)
+    let(:fields) {{'path' => @test_wc_path, 'remote_repo' => "origin", 'branch_name' => "test_branch_#{@dt}"}}
 
-       `cd #{@test_wc_path} && git config user.name Test && git config user.email test@example.com`
+    before(:each) do
+      initial_clone
+      subject.perform(:branch, workitem)
+    end
 
-       workitem['fields']['__error__'].should be_nil
-     end
+    it { expect(subject.error).to be_nil }
+    #it { expect(File.exists?(@test_wc_path)).to be_true }
      
-     it 'should create a new branch' do
-       workitem = {'fields' => {'path' => @test_wc_path, 'remote_repo' => "origin", 'branch_name' => "test_branch_#{@dt}"}}
+    context 'when creating new branch fails' do
+      let(:fields) { super().merge({'path' => "/asdfasd/asdfasd"}) }
 
-       subject.perform(:branch, workitem)
-       
-       workitem['fields']['__error__'].should be_nil
-      # File.exists?(@test_wc_path).should be_true
-     end
-     
-     it 'should detect error if creating new branch fails' do
-       workitem = {'fields' => {'path' => "/asdfasd/asdfasd", 'remote_repo' => "origin", 'branch_name' => "test_branch_#{@dt}"}}
-
-       subject.perform(:branch, workitem)
-
-       workitem['__output__'].should_not include("Pushing to")
-       workitem['__output__'].should include("No such file or directory")
-       workitem['fields']['__error__'].should_not be_nil
-       workitem['fields']['__error__'].should eq('Error creating the branch')
-      # File.exists?(@test_wc_path).should be_false
-     end
+      it 'should detect error' do
+        expect(subject.output).not_to include("Pushing to")
+        expect(subject.output).to include("No such file or directory")
+        expect(subject.error).not_to be_nil
+        expect(subject.error).to eq('Error creating the branch')
+        # expect(File.exists?(@test_wc_path).to be_false
+      end
+    end
   end
 
   describe 'tag()' do
-     before(:each) do
-       FileUtils.rm_rf @test_wc_path
-       workitem = {'fields' => {'branch' => "master", 'path' => @test_wc_path, 'url' => @test_repo_path}}
+    let(:fields) {{'path' => @test_wc_path, 'tag_name' =>"test_tag_v#{@dt}", 'message' => "tag test message", 'remote_repo' => "origin", 'remote_branch' => "master"}}
 
-       subject.perform(:clone, workitem)
+    before(:each) do
+      initial_clone
+      subject.perform(:tag, workitem)
+    end
 
-       `cd #{@test_wc_path} && git config user.name Test && git config user.email test@example.com`
-       workitem['fields']['__error__'].should be_nil
-     end
+    it 'should create a new tag' do
+      expect(subject.error).to be_nil
+    end
      
-     it 'should create a new tag' do
-       workitem = {'fields' => {'path' => @test_wc_path, 'tag_name' =>"test_tag_v#{@dt}", 'message' => "tag test message", 'remote_repo' => "origin", 'remote_branch' => "master"}}
+    context 'when creating new tag fails' do
+      let(:fields) { super().merge({'path' => "/asdfasd/asdfasd"}) }
 
-       subject.perform(:tag, workitem)
-       
-       workitem['fields']['__error__'].should be_nil
-     end
-     
-     it 'should detect error if creating new tag fails' do
-       workitem = {'fields' => {'path' => "/asdfsaf/sdfsfd", 'tag_name' =>"test_tag_v#{@dt}", 'message' => "tag test message", 'remote_repo' => "origin", 'remote_branch' => "master"}}
-
-       subject.perform(:tag, workitem)
-
-       workitem['__output__'].should_not include("Pushing to")
-       workitem['__output__'].should include("No such file or directory")
-       workitem['fields']['__error__'].should_not be_nil
-       workitem['fields']['__error__'].should eq('Error tagging the repo')
-     end
-     
+      it 'should detect error if creating new tag fails' do
+        expect(subject.output).not_to include("Pushing to")
+        expect(subject.output).to include("No such file or directory")
+        expect(subject.error).not_to be_nil
+        expect(subject.error).to eq('Error tagging the repo')
+      end
+    end
   end
-
-  after(:all) do
-    #FileUtils.rm_rf @test_wc_path
-    #FileUtils.rm_rf @test_repo_path
-  end
-
-  
-#  
-#  
-#  
-#  
-#  
-#  
-#  
-#  >>>>>
-#  before(:each) do
-#    FileUtils.rm '/tmp/shell.sh' if File.exists? '/tmp/shell.sh'
-#  end
-#
-#  describe 'valid_workitem?' do
-#    it "should validate fields" do
-#      workitem = {'fields' =>{}}
-#
-#      subject.perform(:execute, workitem)
-#
-#      workitem['fields']['__error__'].should include('missing field command_string')
-#    end
-#  end
-#
-#  describe 'execute' do
-#    before :all do
-##      @workitem =  {'fields' => {'tasks' => '',
-##                                 'path' => @path,
-##                                 'ant_version' => '1.8.2'}}
-#    end
-#
-#    it 'should return successfully with valid command' do
-#      command = 'touch /tmp/archive_test.tar.gz && ls -l /tmp/archive_test.tar.gz'
-#      workitem = {'fields' => {
-#        'command_string' => command, 
-#        'environment' => 'PATH=$PATH;'
-#      }}
-#
-#      subject.perform(:execute, workitem)
-#
-#      workitem['fields']['__error__'].should be_nil
-#      workitem['__output__'].should include("archive_test.tar.gz")
-#    end
-#    
-#    it 'should return successfully with invalid command' do
-#      command = 'ls -l /blahdy/'
-#      workitem = {'fields' => {
-#        'command_string' => command
-#      }}
-#
-#      subject.perform(:execute, workitem)
-#
-#      workitem['fields']['__error__'].should include "No such file or directory"
-#      workitem['__output__'].should include "No such file or directory"
-#    end
-#
-#    it 'should write the output to lucee' do
-#      command = 'echo my message'
-#      workitem = {'fields' => {
-#        'command_string' => command
-#      }}
-#
-#      subject.perform(:execute, workitem)
-#
-#      workitem['fields']['__error__'].should be_nil
-#      workitem['__output__'].should include("my message\n")
-#    end
-#  end
 end
